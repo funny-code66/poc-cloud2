0bb90b3cac5339b0a708fb9e52dbf4189a57f2fc
bright113gene <bright113gene@gmail.com>
2022-12-18 10:39:03 -0800

    update: frontend

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/freshman-tech/news-demo-starter-files
 
 go 1.15
 
-require github.com/joho/godotenv v1.4.0 // indirect
+require (
+	github.com/freshman-tech/news-demo v0.0.0-20201226085618-d28e34153fe9 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,16 +5,65 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
+	"encoding/json"
 
 	"github.com/joho/godotenv"
 )
 
+type PageVariables struct {
+	Date         string
+	Time         string
+}
+
+type TxData struct {
+	Content    string
+}
+
 var tpl = template.Must(template.ParseFiles("index.html"))
 
+func docHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("doc handler")
+	now := time.Now() // find the time right now
+	HomePageVars := PageVariables{ //store the date and time in a struct
+		Date: now.Format("02-01-2006"),
+		Time: now.Format("15:04:05"),
+	}
+	log.Println("get fetch: %v", r.Body)
+	var t TxData
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&t)
+
+	if err != nil {
+		panic(err)
+	}
+	log.Println("get fetch: %v", t)
+
+	// w.Write([]byte("<h1>Hello World!</h1>"))
+	tpl.Execute(w, HomePageVars)
+}
+
+func pdfHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("pdf handler")
+	// now := time.Now() // find the time right now
+	// HomePageVars := PageVariables{ //store the date and time in a struct
+	// 	Date: now.Format("02-01-2006"),
+	// 	Time: now.Format("15:04:05"),
+	// }
+	w.Write([]byte("<h1>Hello World!</h1>"))
+	// tpl.Execute(w, HomePageVars)
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// now := time.Now() // find the time right now
+	// HomePageVars := PageVariables{ //store the date and time in a struct
+	// 	Date: now.Format("02-01-2006"),
+	// 	Time: now.Format("15:04:05"),
+	// }
 	// w.Write([]byte("<h1>Hello World!</h1>"))
 	tpl.Execute(w, nil)
 }
+
 
 func main() {
 	err := godotenv.Load()
@@ -31,5 +80,7 @@ func main() {
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/doc", docHandler)
+	mux.HandleFunc("/pdf", pdfHandler)
 	http.ListenAndServe(":"+port, mux)
 }
