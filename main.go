9c5ee4615974cff3ca6ca6704935ded21fdd6719
bright113gene <bright113gene@gmail.com>
2022-12-18 05:54:18 -0800

    feat: use godotenv

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +13,10 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
