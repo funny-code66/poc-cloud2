1562ad6a74ab2b5b76da96baff51e98bb3a699ad
bright113gene <bright113gene@gmail.com>
2022-12-21 06:24:59 -0800

    update: get token from frontend, create PDF download

--- main.go
+++ main.go
@@ -6,16 +6,22 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/user"
 	"text/template"
 	"time"
+	"path/filepath"
+	"net/url"
 
 	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+
 )
 
 // Scopes: OAuth 2.0 scopes provide a way to limit the amount of access that is granted to an access token.
@@ -30,6 +36,8 @@ var googleOauthConfig = &oauth2.Config{
 const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
 
 var email="NoUser"
+var token oauth2.Token
+var documentId string
 
 func oauthGoogleLogin(w http.ResponseWriter, r *http.Request) {
 	log.Println("----------------oauthGoogleLogin()")
@@ -130,9 +138,57 @@ type PageVariables struct {
 type TxData struct {
 	Title   string
 	Content string
+	DocumentId string
+}
+
+type TokenData struct {
+	AccessToken   string
+	ExpiresIn 		int
+	Scope 				string
+	TokenType 		string
 }
 
 var tpl = template.Must(template.ParseFiles("index.html"))
+
+func signinHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("signin handler")
+
+	log.Printf("get fetch: %v", r.Body)
+	var tokenData TokenData
+	err := json.NewDecoder(r.Body).Decode(&tokenData)
+
+	if err != nil {
+		panic(err)
+	}
+	token.AccessToken = tokenData.AccessToken
+	token.TokenType = tokenData.TokenType
+
+	log.Printf("get fetch: %+v", tokenData)
+	log.Printf("AccessToken: %s", token.AccessToken)
+	log.Printf("TokenType: %s", token.TokenType)
+	log.Printf("RefreshToken: %s", token.RefreshToken)
+
+	// TODO: get userinfo.email and return
+	w.Write([]byte("<h1>Hello World!</h1>"))
+}
+
+func signoutHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("signin handler")
+
+	log.Printf("get fetch: %v", r.Body)
+	err := json.NewDecoder(r.Body).Decode(&token)
+
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("AccessToken: %s", token.AccessToken)
+	log.Printf("TokenType: %s", token.TokenType)
+	log.Printf("RefreshToken: %s", token.RefreshToken)
+
+	// TODO: get userinfo.email and return
+	// w.Write([]byte("<h1>Hello World!</h1>"))
+	tpl.Execute(w, nil)
+}
 
 func docHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("doc handler")
@@ -142,16 +198,17 @@ func docHandler(w http.ResponseWriter, r *http.Request) {
 		Date:  now.Format("02-01-2006"),
 		Time:  now.Format("15:04:05"),
 	}
-	log.Printf("get fetch: %v", r.Body)
+	log.Printf("get fetch: %+v", r.Body)
 	var t TxData
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&t)
+	err := json.NewDecoder(r.Body).Decode(&t)
 
 	if err != nil {
 		panic(err)
 	}
 	log.Printf("Title: %s", t.Title)
 	log.Printf("Content: %s", t.Content)
+	log.Printf("DocumentId: %s", t.DocumentId)
+	documentId = t.DocumentId
 
 	// w.Write([]byte("<h1>Hello World!</h1>"))
 	tpl.Execute(w, HomePageVars)
@@ -164,6 +221,51 @@ func pdfHandler(w http.ResponseWriter, r *http.Request) {
 	// 	Date: now.Format("02-01-2006"),
 	// 	Time: now.Format("15:04:05"),
 	// }
+	ctx := context.Background()
+
+	// process the credential file
+	credential, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+					log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// In order for POST upload attachment to work
+	// You need to authorize the Gmail API v1 scope
+	// at https://developers.google.com/oauthplayground/
+	// otherwise you will get Authorization error in the API JSON reply
+
+	// Use DriveScope for this example. Because of we want to Manage the files in
+	// Google Drive.
+
+	// See the rest at https://godoc.org/google.golang.org/api/drive/v3#pkg-constants
+
+	config, err := google.ConfigFromJSON(credential, drive.DriveScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	client := config.Client(ctx, &token)
+
+	// initiate a new Google Drive service
+	driveClientService, err := drive.New(client)
+	if err != nil {
+		log.Fatalf("Unable to initiate new Drive client: %v", err)
+	}
+
+	mimeType := "application/pdf"
+	filename := "sample.pdf"
+
+	res, err := driveClientService.Files.Export(documentId, mimeType).Download()
+	if err != nil {
+			log.Fatalf("Error: %v", err)
+	}
+	fmt.Printf("File.Export Result: %v", res)
+	file, err := os.Create(filename)
+	if err != nil {
+			log.Fatalf("Error: %v", err)
+	}
+	defer file.Close()
+	_, err = io.Copy(file, res.Body)
 	w.Write([]byte(email))
 	// tpl.Execute(w, HomePageVars)
 }
@@ -178,6 +280,86 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	// w.Write([]byte("<h1>Hello World!</h1>"))
 
 	tpl.Execute(w, HomePageVars)
+}
+
+
+// NOTE : we don't want to visit CSRF URL to get the authorization code
+// and paste into the terminal each time we want to send an email
+// therefore we will retrieve a token for our client, save the token into a file
+// you will be prompted to visit a link in your browser for authorization code only ONCE
+// and subsequent execution of the program will not prompt you for authorization code again
+// until the token expires.
+
+// getClient uses a Context and Config to retrieve a Token
+// then generate a Client. It returns the generated Client.
+func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+					log.Fatalf("Unable to get path to cached credential file. %v", err)
+	}
+	tok, err := tokenFromFile(cacheFile)
+	if err != nil {
+					tok = getTokenFromWeb(config)
+					saveToken(cacheFile, tok)
+	}
+	return config.Client(ctx, tok)
+}
+
+// getTokenFromWeb uses Config to request a Token.
+// It returns the retrieved Token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+					"authorization code: \n%v\n", authURL)
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+					log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(oauth2.NoContext, code)
+	if err != nil {
+					log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+// tokenCacheFile generates credential file path/filename.
+// It returns the generated credential path/filename.
+func tokenCacheFile() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+					return "", err
+	}
+	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
+	os.MkdirAll(tokenCacheDir, 0700)
+	return filepath.Join(tokenCacheDir,
+					url.QueryEscape("google-drive-golang.json")), err
+}
+
+// tokenFromFile retrieves a Token from a given file path.
+// It returns the retrieved Token and any read error encountered.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+					return nil, err
+	}
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
+	return t, err
+}
+
+// saveToken uses a file path to create a file and store the
+// token in it.
+func saveToken(file string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.Create(file)
+	if err != nil {
+					log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
 
 func main() {
@@ -200,6 +382,8 @@ func main() {
 	// OauthGoogle
 	mux.HandleFunc("/auth/google/login", oauthGoogleLogin)
 	mux.HandleFunc("/auth/google/callback/", oauthGoogleCallback)
+	mux.HandleFunc("/signin", signinHandler)
+	mux.HandleFunc("/signout", signoutHandler)
 
 	// FileGoogle
 	mux.HandleFunc("/doc", docHandler)
